a48ed7c385451410549e365c262f1c483165812a
pappanick <nickpappas19@pm.me>
2023-08-02 12:45:00 +0300

    changes locale patchup

--- src/utils/next-i18next.config.ts
+++ src/utils/next-i18next.config.ts
@@ -1,6 +1,10 @@
-export const AppConfig = {
+/** @type {import('next-i18next').AppConfig} */
+module.exports = {
   site_name: 'OP3C React landing page',
   title: 'OP3C React landing page',
   description: "OP3C compression algorithm's landing page",
-  locale: 'el',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'el'],
+  },
 };
